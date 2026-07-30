279f40890ba3d66b3c89cac2d2dcfb2b1f83913e
Muhammad Amin <muhdaminkafri45@gmail.com>
2026-07-30 20:26:51 +0800

    feat: allow cors for local frontend server

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 const cookieParser = require('cookie-parser');
-
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:8080',
+    credentials: true
+}))
 
 app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
